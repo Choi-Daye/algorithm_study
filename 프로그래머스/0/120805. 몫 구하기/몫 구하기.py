1c233a7b862ad597b3fd0bd2af2ee067a4d4e096
Choi-Daye <117803547+Choi-Daye@users.noreply.github.com>
2024-06-11 10:33:35 +0900

    [level 0] Title: 몫 구하기, Time: 0.00 ms, Memory: 10.3 MB -BaekjoonHub

--- 프로그래머스/0/120805. 몫 구하기/몫 구하기.py
+++ 프로그래머스/0/120805. 몫 구하기/몫 구하기.py
@@ -1,3 +1,5 @@
 def solution(num1, num2):
-    answer = num1 // num2
-    return answer
+    if num1 > 0 and num1 <= 100:
+        if num2 > 0 and num2 <= 100:
+            answer = num1 // num2
+            return answer
