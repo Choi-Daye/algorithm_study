733002ede1f93999f13cd2b2e77d27626d70f976
Choi-Daye <117803547+Choi-Daye@users.noreply.github.com>
2025-02-20 21:18:33 +0900

    [level 0] Title: [PCCE 기출문제] 6번 / 물 부족, Time: 0.01 ms, Memory: 10.1 MB -BaekjoonHub

--- 프로그래머스/0/340202. ［PCCE 기출문제］ 6번 ／ 물 부족/［PCCE 기출문제］ 6번 ／ 물 부족.py
+++ 프로그래머스/0/340202. ［PCCE 기출문제］ 6번 ／ 물 부족/［PCCE 기출문제］ 6번 ／ 물 부족.py
@@ -1,7 +1,7 @@
 def solution(storage, usage, change):
     total_usage = 0
     for i in range(len(change)):
-        usage = usage + usage * change[i]/100
+        usage = usage * (1 + change[i]/100)  # usage(1+change[i]/100) 으로 계산시 -> 함수 호출로 인식
         total_usage += usage
         if total_usage > storage:
             return i
