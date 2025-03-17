1f47cfc2371036e955660e90990ff7fd4ff6f16a
Choi-Daye <117803547+Choi-Daye@users.noreply.github.com>
2025-03-18 01:02:05 +0900

    [level 0] Title: 간단한 논리 연산, Time: 0.01 ms, Memory: 9.1 MB -BaekjoonHub

--- 프로그래머스/0/181917. 간단한 논리 연산/간단한 논리 연산.py
+++ 프로그래머스/0/181917. 간단한 논리 연산/간단한 논리 연산.py
@@ -1,5 +1,2 @@
 def solution(x1, x2, x3, x4):
-    X = True if x1 or x2 else False
-    Y = True if x3 or x4 else False
-    answer = True if X and Y else False
-    return answer
+    return (x1 or x2) and (x3 or x4)
