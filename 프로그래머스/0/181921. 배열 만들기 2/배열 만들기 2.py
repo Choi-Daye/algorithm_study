dd3ea78c75f6ca96795afbaa3f7583630965b896
Choi-Daye <117803547+Choi-Daye@users.noreply.github.com>
2025-03-21 17:22:41 +0900

    [level 0] Title: 배열 만들기 2, Time: 496.71 ms, Memory: 9.09 MB -BaekjoonHub

--- 프로그래머스/0/181921. 배열 만들기 2/배열 만들기 2.py
+++ 프로그래머스/0/181921. 배열 만들기 2/배열 만들기 2.py
@@ -1,6 +1,6 @@
 def solution(l, r):
     answer = []
     for i in range(l,r+1) :
-        if set(str(i)) <= {"0", "5"} :
+        if set(str(i)) <= {"0", "5"} :    # 부분집합
             answer.append(i)
     return answer if answer else [-1] 
