cd39716d6070ae8dad6c01b103ba77c7f61094a8
Choi-Daye <117803547+Choi-Daye@users.noreply.github.com>
2025-07-27 14:34:36 +0900

    [level 0] Title: 특별한 이차원 배열 2, Time: 0.43 ms, Memory: 9.55 MB -BaekjoonHub

--- 프로그래머스/0/181831. 특별한 이차원 배열 2/특별한 이차원 배열 2.py
+++ 프로그래머스/0/181831. 특별한 이차원 배열 2/특별한 이차원 배열 2.py
@@ -1,7 +1,6 @@
 def solution(arr):
-    
-    for i in range(len(arr[0])) :
-        for j in range(len(arr[0])) :
+    for i in range(len(arr)) :
+        for j in range(0, i) :
             if arr[i][j] != arr[j][i] :
                 return 0
     return 1
