a2ffb8359db43c4cae8699ac9590e0f9d6f76d34
Choi-Daye <117803547+Choi-Daye@users.noreply.github.com>
2025-08-02 21:14:05 +0900

    [level 0] Title: 원하는 문자열 찾기, Time: 0.13 ms, Memory: 9.32 MB -BaekjoonHub

--- 프로그래머스/0/181878. 원하는 문자열 찾기/원하는 문자열 찾기.py
+++ 프로그래머스/0/181878. 원하는 문자열 찾기/원하는 문자열 찾기.py
@@ -1,6 +1,3 @@
 def solution(myString, pat):
     myString, pat = myString.lower(), pat.lower()
-    if pat in myString :
-        return 1
-    else :
-        return 0
+    return int(pat in myString)
