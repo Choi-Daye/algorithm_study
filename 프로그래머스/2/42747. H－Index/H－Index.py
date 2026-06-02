9052e6e9b87cba86c672e0697f7859741884b80c
Choi-Daye <117803547+Choi-Daye@users.noreply.github.com>
2026-06-02 16:18:37 +0900

    [level 2] Title: H-Index, Time: 0.11 ms, Memory: 11.5 MB -BaekjoonHub

--- 프로그래머스/2/42747. H－Index/H－Index.py
+++ 프로그래머스/2/42747. H－Index/H－Index.py
@@ -1,9 +1,7 @@
 def solution(citations):
-    answer = 0
-    for h in range(0, len(citations)+1):
-        higher = sum(1 for c in citations if c >= h)
-        if higher < h:
-            return answer
-        else:
-            answer = h
-    return answer
+    citations.sort(reverse=True)
+    
+    for i, h in enumerate(citations):
+        if h  < i+1:
+            return i
+    return len(citations)
