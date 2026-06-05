8a19511c75de0dbf94278f197da92b9d003c0115
Choi-Daye <117803547+Choi-Daye@users.noreply.github.com>
2026-06-05 15:37:44 +0900

    [level 2] Title: 짝지어 제거하기, Time: 53.42 ms, Memory: 13.3 MB -BaekjoonHub

---
+++ 프로그래머스/2/12973. 짝지어 제거하기/짝지어 제거하기.py
@@ -0,0 +1,12 @@
+def solution(s):
+    answer = -1
+
+    stack = []
+    
+    for char in s:
+        if stack and stack[-1] == char:
+            stack.pop()
+        else:
+            stack.append(char)
+    
+    return 0 if stack else 1
