74125c251c4d580529b73f448b9c0ff5633ca828
Choi-Daye <117803547+Choi-Daye@users.noreply.github.com>
2026-07-03 19:25:23 +0900

    [level 0] Title: 외계행성의 나이, Time: 0.01 ms, Memory: 11.6 MB -BaekjoonHub

--- 프로그래머스/0/120834. 외계행성의 나이/외계행성의 나이.py
+++ 프로그래머스/0/120834. 외계행성의 나이/외계행성의 나이.py
@@ -1,3 +1,3 @@
 def solution(age):
-    apb = ['a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z']
+    apb = ['a','b','c','d','e','f','g','h','i','j']
     return ''.join(apb[int(a)] for a in str(age))
