a4cedc285232aefd2d3f9f7da84553babac9295e
Choi-Daye <117803547+Choi-Daye@users.noreply.github.com>
2026-07-04 18:26:14 +0900

    [level 0] Title: 숨어있는 숫자의 덧셈 (2), Time: 0.01 ms, Memory: 11.4 MB -BaekjoonHub

--- 프로그래머스/0/120864. 숨어있는 숫자의 덧셈 （2）/숨어있는 숫자의 덧셈 （2）.py
+++ 프로그래머스/0/120864. 숨어있는 숫자의 덧셈 （2）/숨어있는 숫자의 덧셈 （2）.py
@@ -1,15 +1,10 @@
 def solution(my_string):
-    answer = 0
-    num = ''
-    for i, s in enumerate(my_string):
-        if s in ['1','2','3','4','5','6','7','8','9','0']:
-            num += s
-            if i == len(my_string)-1:
-                answer += int(num)
+    answer = ""
+    
+    for s in my_string:
+        if s.isdigit():
+            answer += s
         else:
-            try:
-                answer += int(num)
-                num = ''
-            except Exception:
-                continue
-    return answer
+            answer += ' '
+
+    return sum(int(n) for n in answer.split())
