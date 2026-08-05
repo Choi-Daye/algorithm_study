1d2106828a576b68c1a4d20a347c67aa2b6d60e0
Choi-Daye <117803547+Choi-Daye@users.noreply.github.com>
2026-08-05 16:59:34 +0900

    [level 0] Title: 특이한 정렬, Time: 0.04 ms, Memory: 11.6 MB -BaekjoonHub

--- 프로그래머스/0/120880. 특이한 정렬/특이한 정렬.py
+++ 프로그래머스/0/120880. 특이한 정렬/특이한 정렬.py
@@ -1,20 +1,2 @@
 def solution(numlist, n):
-    answer = []
-    
-    dist = sorted([num - n for num in numlist])
-    
-    m_dist = [d for d in dist if d < 0]
-    p_dist = [d for d in dist if d >= 0]
-    
-    while m_dist or p_dist:
-        if not m_dist:
-            answer.append(p_dist.pop(0) + n)
-        elif not p_dist:
-            answer.append(m_dist.pop() + n)
-        else:
-            if abs(m_dist[-1]) < p_dist[0]:
-                answer.append(m_dist.pop() + n)
-            else:
-                answer.append(p_dist.pop(0) + n)
-    
-    return answer
+    return sorted(numlist, key=lambda x: (abs(x-n), n-x))
