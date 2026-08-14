13bd890e97e2136d624952465300b5e9ee9adc08
Choi-Daye <117803547+Choi-Daye@users.noreply.github.com>
2026-08-14 17:03:27 +0900

    [level 1] Title: 약수의 개수와 덧셈, Time: 0.19 ms, Memory: 11.4 MB -BaekjoonHub

--- 프로그래머스/1/77884. 약수의 개수와 덧셈/약수의 개수와 덧셈.py
+++ 프로그래머스/1/77884. 약수의 개수와 덧셈/약수의 개수와 덧셈.py
@@ -1,19 +1,2 @@
-def divisor(n):
-    num = set()
-    for i in range(1, int(n**0.5)+1):
-        if not n % i:
-            num.add(i)
-            num.add(n//i)
-    return len(num)
-
 def solution(left, right):
-    answer = 0
-    
-    for i in range(left, right+1):
-        if divisor(i) % 2:
-            answer -= i
-        else:
-            answer += i
-
-    return answer
-
+    return sum(-i if int(i**0.5) == i**0.5 else i for i in range(left, right+1))
