c55e0613214deb674014d3e5e19a53fb3c1bcc64
Choi-Daye <117803547+Choi-Daye@users.noreply.github.com>
2026-09-07 18:25:46 +0900

    [level 1] Title: 시저 암호, Time: 0.65 ms, Memory: 11.3 MB -BaekjoonHub

---
+++ 프로그래머스/1/12926. 시저 암호/시저 암호.py
@@ -0,0 +1,15 @@
+def solution(s, n):
+    answer = ''
+    string = "abcdefghijklmnopqrstuvwxyzabcdefghijklmnopqrstuvwxyz"
+    
+    for i in s:
+        if i == ' ':
+            answer += ' '
+        elif i == i.upper():
+            idx = string.find(i.lower())
+            answer += string[idx+n].upper()
+        else:
+            idx = string.find(i)
+            answer += string[idx+n]
+            
+    return answer
