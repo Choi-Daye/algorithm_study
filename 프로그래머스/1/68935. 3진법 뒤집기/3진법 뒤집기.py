d8e9e5d51e0d6337cd0cb4e0b958c105450f1e92
Choi-Daye <117803547+Choi-Daye@users.noreply.github.com>
2026-09-10 20:02:21 +0900

    [level 1] Title: 3진법 뒤집기, Time: 0.01 ms, Memory: 11.7 MB -BaekjoonHub

--- 프로그래머스/1/68935. 3진법 뒤집기/3진법 뒤집기.py
+++ 프로그래머스/1/68935. 3진법 뒤집기/3진법 뒤집기.py
@@ -1,9 +1,8 @@
 def solution(n):
     answer = ''
 
-    while n >= 3:
+    while n:
         answer += str(n%3)
         n //= 3
         
-    answer += str(n)
     return sum(3**i * int(num) for i, num in enumerate(answer[::-1]))
